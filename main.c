56fe666e0ae4ac35b847ee3361381d47a7a335fd
Chua Hou <human+github@chuahou.dev>
2021-07-25 14:11:22 +0800

    version: bump to v0.1.1.1

--- main.c
+++ main.c
@@ -43,7 +43,7 @@ int main(int argc, char **argv)
 {
 	int code = EXIT_FAILURE;
 	
-	FLUSHPUTS("steam-webgame-launcher v0.1.1.0-git");
+	FLUSHPUTS("steam-webgame-launcher v0.1.1.1");
 	
 	// Check we got exactly 3 arguments---
 	// argv[1]: the URL of the webgame.
